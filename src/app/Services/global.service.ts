34b0f6934a00e9a3e288e6d127c8f3c3e8dbd022
saifasghar <mixture030030@gmail.com>
2023-06-04 18:16:46 +0500

    wotking with socket-io

--- src/app/Services/global.service.ts
+++ src/app/Services/global.service.ts
@@ -2,15 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from 'src/app/util/models/api-response';
 import { Injectable } from '@angular/core';
+import { io, Socket } from "socket.io-client";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
 
-  constructor(private http: HttpClient) { }
+  private socket: Socket;
+
+  constructor(private http: HttpClient) {
+    this.socket = io("http://localhost:3000");
+  }
 
   verifyTokenAuthenticity(): Observable<ApiResponse<any>> {
     return this.http.get<any>('auth/verify-token');
+  }
+
+  sendMessage(message: string): void {
+    this.socket.emit("chat message", message);
+  }
+
+  receiveMessage(): Observable<string> {
+    return new Observable<string>((observer) => {
+      this.socket.on("chat message", (message: string) => {
+        observer.next(message);
+      });
+    });
   }
 }
