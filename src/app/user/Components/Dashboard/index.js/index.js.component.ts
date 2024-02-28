e1f4de4b3bbdbd49c7b91a60446792880a4c7097
saif-asghar <saif14497@gmail.com>
2024-02-28 21:03:07 +0500

    fix

--- src/app/user/Components/Dashboard/index.js/index.js.component.ts
+++ src/app/user/Components/Dashboard/index.js/index.js.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { UserService } from 'src/app/user/Services/user.service';
+import { StorageObserver } from 'src/app/util/storage.observer';
+import { selectedTabs } from 'src/app/util/models/selectedTabs';
 
 @Component({
   selector: 'app-index.js',
@@ -12,46 +15,115 @@ export class IndexJsComponent {
   constructor(
     private userServices: UserService,
     private modalService: BsModalService,
+    private storageObserver: StorageObserver,
+    private router: Router,
   ) { }
 
   @ViewChild('profileModalTemplate') profileModalTemplate: any;
+  @ViewChild('mediaModalTemplate') mediaModalTemplate: any;
 
   public profileModalRef: BsModalRef;
-  public curropenedStuff: {
-    sideBar: string,
-    chat: string,
-    chatInfo: boolean
-  } = {
-      sideBar: 'messages',
-      chat: 'single',
-      chatInfo: false
-    };
+  public mediaModalRef: BsModalRef;
+  public openedMediaImage: any = {};
+  public allFriends: any = null;
+  public allNotifications: any = null;
+  public allFriendRequests: any = null;
+  public mediaImages: any = [
+    {
+      src: 'assets/img/chat/1.jpg'
+    },
+    {
+      src: 'assets/img/chat/2.jpg'
+    },
+    {
+      src: 'assets/img/chat/3.jpg'
+    },
+    {
+      src: 'assets/img/chat/4.jpg'
+    },
+    {
+      src: 'assets/img/chat/5.jpg'
+    },
+    {
+      src: 'assets/img/chat/6.jpg'
+    },
+    {
+      src: 'assets/img/chat/7.jpg'
+    },
+    {
+      src: 'assets/img/chat/8.jpg'
+    },
+    {
+      src: 'assets/img/chat/9.jpg'
+    },
+  ];
+  public curropenedStuff: selectedTabs = {
+    sideBar: 'messages',
+    chat: 'single',
+    chatInfo: false,
+    infoTabSelected: 'offcanvas-tab-profile'
+  };
 
-  openModal(template: any) {
-    this.profileModalRef = this.modalService.show(template);
+  openModal(template: any, type: string = 'profile', image: any = {}) {
+    if (type == 'profile') {
+      this.profileModalRef = this.modalService.show(template);
+    } else if (type == 'media') {
+      this.openedMediaImage = image;
+      this.mediaModalRef = this.modalService.show(template);
+    }
+  }
+
+  changeCurrOpenedSideBar(type: string) {
+    if (type == 'createChat') {
+
+    } else if (type == 'friends' && !this.allFriends) {
+      this.allFriends = null;
+      this.fetchSideBarData(type);
+    } else if (type == 'friendRequests' && !this.allFriendRequests) {
+      this.fetchSideBarData(type);
+    } else if (type == 'messages') {
+
+    } else if (type == 'notifications' && this.curropenedStuff.sideBar !== 'notifications') {
+      this.allNotifications = null;
+      this.fetchSideBarData(type);
+    } else if (type == 'support') {
+
+    } else if (type == 'settings') {
+
+    }
+    this.curropenedStuff.sideBar = type;
+  }
+
+  fetchSideBarData(type: string) {
+    if (type == 'friends') {
+      this.userServices.fetchAllFriends().subscribe(response => {
+        if (response.success) {
+          this.allFriends = response.data.friends;
+        }
+      });
+    } else if (type == 'notifications') {
+      this.userServices.fetchAllNotifications().subscribe(response => {
+        console.log(response);
+        if (response.success) {
+          this.allNotifications = response.data.notifications.reverse();
+        }
+      });
+    } else if (type == 'friendRequests') {
+      this.userServices.fetchAlFriendRequests().subscribe(response => {
+        console.log(response);
+        if (response.success) {
+          this.allFriendRequests = response.data.requests.reverse();
+        }
+      });
+    }
+  }
+
+  logoutUser() {
+    this.profileModalRef.hide();
+    this.storageObserver.deleteAllCookies();
+    this.router.navigate(['/auth/login']);
   }
 
   ngOnInit() {
-    // setTimeout(() => {
-    //   console.log(this.profileModalTemplate);
-    //   console.log(this.profileModalRef);
-    //   this.profileModalRef = this.modalService.show(this.profileModalTemplate);
-    //   console.log(this.profileModalRef);
-    //   setTimeout(() => {
-    //     this.modalService.hide();
-    //     setTimeout(() => {
-    //       this.modalService.show(this.profileModalTemplate);
-    //     }, 2000)
-    //   }, 2000)
-    // }, 2000);
-    // this.userServices.test().subscribe(response => {
-    //   if (response.success) {
-    //     console.log(response);
-    //   }
-    // });
-  }
-
-  logout() {
-
   }
 }
